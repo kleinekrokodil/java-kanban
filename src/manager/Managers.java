7e81e997ed35105bdc23229872a331b12d24832c
Vladimir Kibirev <kaiservladimir@gmail.com>
2026-05-21 01:47:13 +0800

    Исправил Managers.getDefault();

--- src/manager/Managers.java
+++ src/manager/Managers.java
@@ -1,14 +1,12 @@
 package manager;
 
-import java.io.File;
-
 public class Managers {
     private Managers() {
 
     }
 
     public static TaskManager getDefault() {
-        return FileBackedTaskManager.loadFromFile(new File("backup.csv"));
+        return new InMemoryTaskManager();
     }
 
     public static HistoryManager getDefaultHistory() {
